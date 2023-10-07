0675938e5e91f29746ac8ba6a48c06ad6e45a101
maryclaireteahan <maryclaireteahan@gmail.com>
2023-10-07 16:24:44 +0100

    save

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
@@ -6,14 +6,29 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results)
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const mapResults2 = nums.map(function (num) {return num * 2;})
+console.log(mapResults2);
 
 // Simplfied w/ map() + arrow function
+ 
+const arrow = nums.map(num => num * 2)
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +52,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(students => [students.name, students.id]);
+console.log(studentsWithIds);
